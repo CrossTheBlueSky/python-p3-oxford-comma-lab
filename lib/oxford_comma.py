01b990d73ff9c6dcd8a0c3bd226e9f1e30b39e7c
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-11 16:20:56 -0700

    bruh

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        joined = ",-".join(items)
+        separated = joined.split("-")
+        separated.insert(-1, "and")
+        return " ".join(separated)
+    elif len(items) == 2:
+        items.insert(-1, "and")
+        return " ".join(items)
+    else:
+        return items[0]
+
